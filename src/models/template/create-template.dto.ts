03cae152f34e4f75053cde528f66a5659d62434d
coby111 <sorian11mor@gmail.com>
2024-06-27 20:14:19 -0600

    Updated DTOs for Template: CreateTemplateDto and UpdateTemplateDto

--- src/models/template/create-template.dto.ts
+++ src/models/template/create-template.dto.ts
@@ -1,1 +1,7 @@
-export class CreateTemplateDto {}
+export class CreateTemplateDto {
+  state: string = 'pendiente';
+  areaId: number;
+  period: string;
+  responsibleId: number;
+  revisedById: number;
+}
--- src/templates/templates.controller.ts
+++ src/templates/templates.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { TemplatesService } from './templates.service';
 import { CreateTemplateDto } from './dto/create-template.dto';
 import { UpdateTemplateDto } from './dto/update-template.dto';
@@ -23,7 +31,10 @@ export class TemplatesController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateTemplateDto: UpdateTemplateDto) {
+  update(
+    @Param('id') id: string,
+    @Body() updateTemplateDto: UpdateTemplateDto,
+  ) {
     return this.templatesService.update(+id, updateTemplateDto);
   }
 
