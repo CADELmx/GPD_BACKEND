5e9521be7ab27e38f041fccc1c3b5f05ea1f131e
coby111 <sorian11mor@gmail.com>
2024-06-27 23:36:30 -0600

    Added documentation to CRUD for templates in controller file

--- src/templates/templates.controller.ts
+++ src/templates/templates.controller.ts
@@ -15,21 +15,41 @@ import { TemplatesService } from './templates.service';
 export class TemplatesController {
   constructor(private readonly templatesService: TemplatesService) {}
 
+  /**
+   * Creates a new template
+   * @param {Prisma.TemplateCreateInput} createTemplateDto The template data transfer object
+   * @returns - The created template
+   */
   @Post()
   create(@Body() createTemplateDto: Prisma.TemplateCreateInput) {
     return this.templatesService.create({ ...createTemplateDto });
   }
 
+  /**
+   * Retrieves all templates
+   * @returns . An array of all templates
+   */
   @Get()
   findAll() {
     return this.templatesService.findAll();
   }
 
+  /**
+   * Retrieves a template by ID
+   * @param {number} id - The ID of the template to retrieve
+   * @returns - The template with the specified ID
+   */
   @Get(':id')
   findOne(@Param('id', ParseIntPipe) id: number) {
     return this.templatesService.findOne(id);
   }
 
+  /**
+   * Updates a template by ID
+   * @param {number} id - The ID of the template to update
+   * @param {Prisma.TemplateUpdateInput} updateTemplateDto - The updated template data traansfer object
+   * @returns - The updated template
+   */
   @Patch(':id')
   update(
     @Param('id', ParseIntPipe) id: number,
@@ -38,6 +58,11 @@ export class TemplatesController {
     return this.templatesService.update(id, updateTemplateDto);
   }
 
+  /**
+   * Deletes a template by ID
+   * @param {number} id - The ID of the template to delete
+   * @returns - The deleted template
+   */
   @Delete(':id')
   remove(@Param('id', ParseIntPipe) id: number) {
     return this.templatesService.remove(id);
