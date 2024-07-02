e9815ecee20b4c109c60cf12a8c1258fb02f891f
Elisa <113410035+elisa242003@users.noreply.github.com>
2024-07-01 21:16:42 -0600

    Feature/partial template (#20)
    * Implementation of search for everyone and single user
    
    * Implementation of registration of a new partial template
    
    * Function to edit a Partial Template
    
    * Function to delete a Partial Template by means of its id
    
    * Feature update
    
    * Add comments to service and controller functions
    
    * Changes implemented on the pull request observations

---
+++ src/models/partialTemplate/update-partial-template.dto.ts
@@ -0,0 +1,12 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreatePartialTemplateDto } from './create-partial-template.dto';
+
+export class UpdatePartialTemplateDto extends PartialType(CreatePartialTemplateDto) {
+  name?: string
+  gender?: string
+  position?: string
+  total?: number
+  status?: string
+  year?: string
+  period?: string
+}
