6b210cf0557f01bac3b7c254bd00fccf7608a4c5
Diego López Ariza <diegoawesome2017@outlook.com>
2024-07-10 12:45:07 -0600

    Created subject module
    - created service
    - created controller

--- src/subject/subject.controller.ts
+++ src/subject/subject.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post, Put } from '@nestjs/common';
+import { SubjectService } from './subject.service';
+import { CreateSubjectDto } from 'src/models/subject/create-subject.dto';
+import { UpdateSubjectDto } from 'src/models/subject/update-subject.dto';
 
 @Controller('subject')
-export class SubjectController {}
+export class SubjectController {
+    constructor(private readonly subjectService: SubjectService) {}
+
+    @Post()
+    create(@Body() createSubjectDto: CreateSubjectDto) {
+        return this.subjectService.create(createSubjectDto);
+    }
+
+    @Get()
+    findAll() {
+        return this.subjectService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.subjectService.findOne(id);
+    }
+
+    @Get('program/:id')
+    findByProgram(@Param('id', ParseIntPipe) id: number) {
+        return this.subjectService.findByProgram(id);
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateSubjectDto: UpdateSubjectDto) {
+        return this.subjectService.update(id, updateSubjectDto);
+    }
+}
--- src/subject/subject.service.ts
+++ src/subject/subject.service.ts
@@ -1,20 +1,64 @@
-import { Injectable } from "@nestjs/common";
+import { HttpException, Injectable, NotFoundException } from "@nestjs/common";
 import { Subject } from "@prisma/client";
 import { validateForeignKeys } from "src/common/validation/custom-validation.pipe";
 import { CreateSubjectDto } from "src/models/subject/create-subject.dto";
+import { UpdateSubjectDto } from "src/models/subject/update-subject.dto";
 import { PrismaService } from "src/prisma.service";
 
 @Injectable()
 export class SubjectService {
     constructor(private readonly prisma: PrismaService, private readonly foreign: validateForeignKeys) { }
-    async create(createSubjectDto: CreateSubjectDto): Promise<Subject> {
+    async create(createSubjectDto: CreateSubjectDto): Promise<{ message: string }> {
 
-        this.foreign.add(this.prisma.educationalPrograms.count({where: {id: createSubjectDto.educationalProgramId}}))
+        this.foreign.add(this.prisma.educationalPrograms.count({ where: { id: createSubjectDto.educationalProgramId } }))
         this.foreign.validate()
-        return await this.prisma.subject.create({
+        const created = await this.prisma.subject.create({
             data: {
                 ...createSubjectDto,
             },
         });
+        if (!created) throw new HttpException("Error al registrar materia", 500);
+        return {
+            message: "Registrado!"
+        };
+    }
+    async findByProgram(id: number): Promise<Subject[]> {
+        const subjects = await this.prisma.subject.findMany({
+            where: {
+                educationalProgramId: id
+            },
+            orderBy: { monthPeriod: 'asc', subjectName: 'asc' }
+        });
+        if (!subjects) throw new NotFoundException("No se encontraron materias");
+        return subjects;
+    }
+    async findOne(id: number): Promise<Subject> {
+        const subject = await this.prisma.subject.findUnique({
+            where: {
+                id
+            }
+        });
+        if (!subject) throw new NotFoundException("No se encontró la materia");
+        return subject;
+    }
+    async findAll(): Promise<Subject[]> {
+        const subjects = await this.prisma.subject.findMany({ orderBy: { educationalProgramId: 'asc', monthPeriod: 'asc', subjectName: 'asc' } });
+        if (!subjects) throw new NotFoundException("No se encontraron materias");
+        return subjects;
+    }
+    async update(id: number, updateSubjectDto: UpdateSubjectDto): Promise<{ message: string }> {
+        this.findOne(id);
+        const updated = await this.prisma.subject.update({
+            where: {
+                id
+            },
+            data: {
+                ...updateSubjectDto
+            }
+        });
+        if (!updated) throw new HttpException("Error al actualizar materia", 500);
+        return {
+            message: "Actualizado!"
+        };
     }
 }
