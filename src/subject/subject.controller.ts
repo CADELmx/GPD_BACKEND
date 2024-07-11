6111fc074054521ac512e73faf9a2d95f0cfeff5
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-07-10 23:29:11 -0600

    Subjects module added (#18)
    * Added subject module
    
    * Created subject module
    
    - created service
    - created controller
    
    * Created controller
    
    - Controller created and fully configured
    - Service updated to implement try-catch in each request
    - interface implemented for subjects service
    
    * Added JSDoc
    
    - added jsdoc to service
    - added jsdoc to controller

---
+++ src/subject/subject.controller.ts
@@ -0,0 +1,54 @@
+import { Body, Controller, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
+import { SubjectService } from './subject.service';
+import { CreateSubjectDto } from 'src/models/subject/create-subject.dto';
+import { UpdateSubjectDto } from 'src/models/subject/update-subject.dto';
+
+@Controller('subject')
+export class SubjectController {
+    constructor(private readonly subjectService: SubjectService) {}
+    /**
+     * Handles the post request to create a new subject
+     * @param createSubjectDto 
+     * @returns 
+     */
+    @Post()
+    create(@Body() createSubjectDto: CreateSubjectDto) {
+        return this.subjectService.create(createSubjectDto);
+    }
+    /**
+     * Handles the get request to get all subjects
+     * @returns 
+     */
+    @Get()
+    findAll() {
+        return this.subjectService.findAll();
+    }
+    /**
+     * Handles the get request to get a subject by id
+     * @param id 
+     * @returns 
+     */
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number) {
+        return this.subjectService.findOne(id);
+    }
+    /**
+     * Handles the get request to get all subjects by program id
+     * @param id 
+     * @returns 
+     */
+    @Get('program/:id')
+    findByProgram(@Param('id', ParseIntPipe) id: number) {
+        return this.subjectService.findByProgram(id);
+    }
+    /**
+     * Handles the patch request to update a subject
+     * @param id 
+     * @param updateSubjectDto 
+     * @returns 
+     */
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() updateSubjectDto: UpdateSubjectDto) {
+        return this.subjectService.update(id, updateSubjectDto);
+    }
+}
