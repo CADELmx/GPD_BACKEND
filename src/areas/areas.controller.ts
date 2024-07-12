284b95b17e877e0c4da9bcdb377d0b5650dbb602
coby111 <sorian11mor@gmail.com>
2024-07-12 12:10:42 -0600

    Fixed simple syntax errors with Eslint

--- src/areas/areas.controller.ts
+++ src/areas/areas.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AreasService } from './areas.service';
 import { CreateAreaDto } from './dto/create-area.dto';
 import { UpdateAreaDto } from './dto/update-area.dto';
