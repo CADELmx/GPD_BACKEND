53caee2342089abf78dcb9bc2c7032db3ddf7ed1
coby111 <sorian11mor@gmail.com>
2024-07-12 12:44:09 -0600

    Added validation to createDto with custom constraints and messages

--- src/models/area/create-area.dto.ts
+++ src/models/area/create-area.dto.ts
@@ -1,1 +1,6 @@
-export class CreateAreaDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+export class CreateAreaDto {
+  @IsString({ message: 'El campo nombre tiene que ser una cadena de texto' })
+  @IsNotEmpty({ message: 'El campo nombre no puede estar vacío' })
+  name: string;
+}
