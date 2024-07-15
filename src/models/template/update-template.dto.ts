e71a041a0822f5b857f42fffd40a907fdd8f2a07
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2024-07-14 22:26:07 -0600

    Revert "Implementation of restrictions, validations and custom messages"

--- src/models/template/update-template.dto.ts
+++ src/models/template/update-template.dto.ts
@@ -1,38 +1,10 @@
-import { IsInt, IsNotEmpty, IsOptional, IsString, Min } from 'class-validator';
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateTemplateDto } from './create-template.dto';
 
-export class UpdateTemplateDto {
-  @IsOptional()
-  @IsString({ message: 'El campo estado tiene que ser una cadena de texto' })
-  @IsNotEmpty({ message: 'El campo estado no puede estar vacio' })
+export class UpdateTemplateDto extends PartialType(CreateTemplateDto) {
   state?: string;
-
-  @IsOptional()
-  @IsInt({ message: "El campo 'área' tiene que ser un numero entero" })
-  @IsNotEmpty({ message: "El campo 'área' no puede estar vacio" })
-  @Min(1, {
-    message: "El campo 'área' tiene que ser un número entero positivo",
-  })
   areaId?: number;
-
-  @IsOptional()
-  @IsString({ message: 'El campo periodo tiene que ser una cadena de texto' })
-  @IsNotEmpty({ message: 'El campo periodo no puede estar vacio' })
   period?: string;
-
-  @IsOptional()
-  @IsInt({ message: "El campo 'responsable' tiene que ser un numero entero" })
-  @IsNotEmpty({ message: "El campo 'responsable' id no puede estar vacio" })
-  @Min(1, {
-    message:
-      "El campo 'responsable' id tiene que ser un número entero positivo",
-  })
   responsibleId?: number;
-
-  @IsOptional()
-  @IsInt({ message: "El campo 'revisado por' tiene que ser un numero entero" })
-  @IsNotEmpty({ message: "El campo 'revisado por' no puede estar vacio" })
-  @Min(1, {
-    message: "El campo 'revisado por' tiene que ser un número entero positivo",
-  })
   revisedById?: number;
 }
--- src/templates/templates.service.ts
+++ src/templates/templates.service.ts
@@ -9,206 +9,85 @@ export class TemplatesService {
   constructor(private readonly prisma: PrismaService) {}
 
   /**
-   * Registers a new template.
-   * @param {CreateTemplateDto} createTemplateDto - The template data to register.
-   * @returns {Promise<{ message: string; error: string | null; data: Template | null }>} - The registered template.
+   * Rregisters a new template
+   * @param {CreateTemplateDto} createTemplate - The template data to register
+   * @returns {Promise<Template>} - The registered template
    */
-  async create(createTemplateDto: CreateTemplateDto): Promise<any> {
-    try {
-      await this.validateAreaId(createTemplateDto);
-      await this.validateResponsibleId(createTemplateDto);
-      await this.validateRevisedById(createTemplateDto);
+  async create(createTemplateDto: CreateTemplateDto): Promise<Template> {
+    await this.validateForeignKeys(createTemplateDto);
 
-      const template = await this.prisma.template.create({
-        data: {
-          ...createTemplateDto,
-        },
-      });
-      return {
-        message: 'Plantilla registrada',
-        error: null,
-        data: template,
-      };
-    } catch (error) {
-      return {
-        message: 'Error al registrar la plantilla',
-        error: error.message,
-        data: null,
-      };
-    }
+    return await this.prisma.template.create({
+      data: {
+        ...createTemplateDto,
+      },
+    });
   }
 
   /**
-   * Lists all templates.
-   * @returns {Promise<{ message: string; error: string | null; data: Template[] | null }>} - All registered templates.
+   * Lists all templates
+   * @returns {Promise<Template[]>} - All registered templates
    */
-  async findAll(): Promise<any> {
-    // return await this.prisma.personalData.findMany();
-    try {
-      const templates = await this.prisma.template.findMany();
-      if (templates.length === 0) {
-        return {
-          message: 'No se encontraron plantillas',
-          error: null,
-          data: [],
-        };
-      }
-
-      return {
-        message: 'Plantillas obtenidas con exito',
-        error: null,
-        data: templates,
-      };
-    } catch (error) {
-      return {
-        message: 'Error al obtener las plantillas',
-        error: error.message,
-        data: null,
-      };
-    }
+  async findAll(): Promise<Template[]> {
+    return await this.prisma.template.findMany();
   }
 
   /**
-   * Gets a template by its ID.
-   * @param {number} id - The ID of the template to search for.
-   * @returns {Promise<{ message: string; error: string | null; data: Template | null }>} - The template based on the given ID.
+   * Gets a template by its ID
+   * @param {number} id - Template ID to search for
+   * @returns {Promise<Template>} -  The template based on the given ID
    */
-  async findOne(id: number): Promise<any> {
-    try {
-      if (id) {
-        await this.validateId(id);
-        const template = await this.prisma.template.findMany({
-          where: {
-            id,
-          },
-        });
-
-        if (template.length === 0) {
-          return {
-            message: 'Aún no existen plantillas para este área',
-            error: null,
-            data: template,
-          };
-        }
-        return {
-          message: 'Plantillas obtenidas con éxito',
-          error: null,
-          data: template,
-        };
-      }
-    } catch (error) {
-      return {
-        message: 'Error al obtener la plantilla',
-        error: error.message,
-        data: null,
-      };
-    }
+  async findOne(id: number): Promise<Template> {
+    await this.validateId(id);
+    return await this.prisma.template.findUnique({
+      where: {
+        id,
+      },
+    });
   }
 
   /**
-   * Updates a template by its ID.
-   * @param {number} id - The ID of the template to update.
-   * @param {UpdateTemplateDto} updateTemplateDto - The template data to update.
-   * @returns {Promise<{ message: string; error: string | null; data: Template | null }>} - The updated template.
+   * Updates a template by its ID
+   * @param {number} id - ID of template to update
+   * @param {UpdateTemplateDto} updateTemplate - Template data to update
+   * @returns {Promise<Template>} - The updated template
    */
-  async update(id: number, updateTemplateDto: UpdateTemplateDto): Promise<any> {
-    try {
-      await this.validateId(id);
-      await this.validateAreaId(updateTemplateDto);
-      await this.validateResponsibleId(updateTemplateDto);
-      await this.validateRevisedById(updateTemplateDto);
+  async update(
+    id: number,
+    updateTemplateDto: UpdateTemplateDto,
+  ): Promise<Template> {
+    await this.validateId(id);
+    await this.validateForeignKeys(updateTemplateDto);
 
-      const template = await this.validateId(id);
-      const currentPeriods = [
-        TemplatesService.currentPeriod(true),
-        TemplatesService.currentPeriod(false),
-      ];
-
-      if (!currentPeriods.includes(template.period)) {
-        return {
-          message: null,
-          error: 'Esta plantilla no se puede actualizar',
-          data: null,
-        };
-      }
-
-      const updatedTemplate = await this.prisma.template.update({
-        where: {
-          id,
-        },
-        data: {
-          ...updateTemplateDto,
-        },
-      });
-
-      return {
-        message: 'Plantilla actualizada',
-        error: null,
-        data: updatedTemplate,
-      };
-    } catch (error) {
-      return {
-        message: 'Error al actualizar la plantilla',
-        error: error.message,
-        data: null,
-      };
-    }
+    const updateTemplate = await this.prisma.template.update({
+      where: {
+        id,
+      },
+      data: { ...updateTemplateDto },
+    });
+    return updateTemplate;
   }
 
   /**
-   * Deletes a template by its ID.
-   * @param {number} id - The ID of the template to delete.
-   * @returns {Promise<{ message: string; error: string | null; data: null }>} - The result of the deletion.
+   * Deletes a template by its ID
+   * @param {number} id - ID of the template to delete
+   * @returns {Promise<Template>} - The deleted template
    */
-  async remove(id: number): Promise<any> {
-    try {
-      await this.validateId(id);
-      await this.prisma.template.delete({
-        where: {
-          id,
-        },
-      });
-      return {
-        message: 'Se eliminó la plantilla',
-        error: null,
-        data: null,
-      };
-    } catch (error) {
-      return {
-        message: 'Error al eliminar la plantilla',
-        error: error.message,
-        data: null,
-      };
-    }
+  async remove(id: number): Promise<Template> {
+    await this.validateId(id);
+    return await this.prisma.template.delete({
+      where: {
+        id,
+      },
+    });
   }
 
   /**
-   * Gets the current period based on the current month and year.
-   * @param {boolean} ord - Indicates whether the period is Ordinary (true) or Extraordinary (false).
-   * @returns {string} - The period string for the current period.
+   * Validates if template ID exists
+   * @param {number} id - Template ID to validate
+   * @returns {Promise<Template>} - The template with the given ID
+   * @throws {NotFoundException} - If the template ID is not found
    */
-  private static currentPeriod(ord: boolean): string {
-    const date = new Date();
-    const year = date.getFullYear();
-    const month = date.getMonth() + 1;
-    const ordinary = ord ? 'Ordinario' : 'Extraordinario';
-
-    if (month <= 4) {
-      return `enero - abril ${year}: ${ordinary}`;
-    } else if (month <= 8) {
-      return `mayo - agosto ${year}: ${ordinary}`;
-    } else {
-      return `septiembre - diciembre ${year}: ${ordinary}`;
-    }
-  }
-
-  /**
-   * Validates if a template ID exists.
-   * @param {number} id - The ID of the template to validate.
-   * @returns {Promise<Template>} - The template with the given ID.
-   * @throws {NotFoundException} - If the template ID is not found.
-   */
-  private async validateId(id: number): Promise<Template> {
+  async validateId(id: number): Promise<Template> {
     const template = await this.prisma.template.findUnique({
       where: {
         id,
@@ -216,70 +95,59 @@ export class TemplatesService {
     });
 
     if (!template) {
-      throw new NotFoundException('Plantilla no encontrada');
+      throw new NotFoundException('Template not found');
     }
     return template;
   }
 
   /**
-   * Validates if the area ID exists.
-   * @param {CreateTemplateDto | UpdateTemplateDto} dto - The DTO containing the area ID.
-   * @returns {Promise<void>} - Resolves if the area ID is valid.
-   * @throws {NotFoundException} - If the area ID is not found.
+   * Validates if foreign keys (areaId, responsibleId, revisedById)
+   * @param {CreateTemplateDto | UpdateTemplateDto} dto - Data to validate
+   * @throws {NotFoundException} - If any foreign keys is not found
    */
-  private async validateAreaId(
+  private async validateForeignKeys(
     dto: CreateTemplateDto | UpdateTemplateDto,
   ): Promise<void> {
-    const { areaId } = dto;
+    const { areaId, responsibleId, revisedById } = dto;
+    const validations = [];
+
     if (areaId !== undefined) {
-      const area = await this.prisma.area.findUnique({ where: { id: areaId } });
-      if (!area) {
-        throw new NotFoundException(`Área con ID ${areaId} no encontrada`);
-      }
+      validations.push(
+        this.prisma.area.count({ where: { id: areaId } }).then((count) => {
+          if (count === 0) {
+            throw new NotFoundException(`Area with ID ${areaId} not found`);
+          }
+        }),
+      );
     }
-  }
 
-  /**
-   * Validates if the responsible user ID exists.
-   * @param {CreateTemplateDto | UpdateTemplateDto} dto - The DTO containing the responsible user ID.
-   * @returns {Promise<void>} - Resolves if the responsible user ID is valid.
-   * @throws {NotFoundException} - If the responsible user ID is not found.
-   */
-  private async validateResponsibleId(
-    dto: CreateTemplateDto | UpdateTemplateDto,
-  ): Promise<void> {
-    const { responsibleId } = dto;
     if (responsibleId !== undefined) {
-      const user = await this.prisma.users.findUnique({
-        where: { nt: responsibleId },
-      });
-      if (!user) {
-        throw new NotFoundException(
-          `Usuario con NT ${responsibleId} no encontrado`,
-        );
-      }
+      validations.push(
+        this.prisma.users
+          .count({ where: { id: responsibleId } })
+          .then((count) => {
+            if (count === 0) {
+              throw new NotFoundException(
+                `User with NT ${responsibleId} not found`,
+              );
+            }
+          }),
+      );
     }
-  }
 
-  /**
-   * Validates if the revised by user ID exists.
-   * @param {CreateTemplateDto | UpdateTemplateDto} dto - The DTO containing the revised by user ID.
-   * @returns {Promise<void>} - Resolves if the revised by user ID is valid.
-   * @throws {NotFoundException} - If the revised by user ID is not found.
-   */
-  private async validateRevisedById(
-    dto: CreateTemplateDto | UpdateTemplateDto,
-  ): Promise<void> {
-    const { revisedById } = dto;
     if (revisedById !== undefined) {
-      const user = await this.prisma.users.findUnique({
-        where: { nt: revisedById },
-      });
-      if (!user) {
-        throw new NotFoundException(
-          `Usuario con NT ${revisedById} no encontrado`,
-        );
-      }
+      validations.push(
+        this.prisma.users
+          .count({ where: { nt: revisedById } })
+          .then((count) => {
+            if (count === 0) {
+              throw new NotFoundException(
+                `User with NT ${revisedById} not found`,
+              );
+            }
+          }),
+      );
     }
+    await Promise.all(validations);
   }
 }
