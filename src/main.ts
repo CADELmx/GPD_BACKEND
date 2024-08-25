6dcc59cead95cf15a9b663910c17f2dc733974a0
Ana202016 <122801690+Ana202016@users.noreply.github.com>
2024-08-24 22:34:33 -0600

    Actualizacion

--- src/main.ts
+++ src/main.ts
@@ -9,15 +9,14 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT || 3000);
 
   const config = new DocumentBuilder()
-  .setTitle('GPD API')
-  .setDescription('Descirpción de uso de la API del proyecto GPD')
-  .setVersion('1.0')
-  .addTag('GPD')
-  .addBearerAuth()
-  .build();
+    .setTitle('GPD API')
+    .setDescription('Descripción de uso de la API del proyecto GPD')
+    .setVersion('1.0')
+    .addTag('GPD')
+    .addBearerAuth()
+    .build();
   
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
@@ -25,6 +24,9 @@ async function bootstrap() {
   app.useGlobalInterceptors(new BigIntInterceptor());
   app.useGlobalPipes(new CustomValidationPipe());
   app.useGlobalFilters(new CustomExceptionFilter());
-  await app.listen(3000);
+
+  await app.listen(process.env.PORT || 3000);  // Solo se llama una vez
 }
+
 bootstrap();
+
