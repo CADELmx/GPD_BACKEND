162ad1617be2ab20db471a644d53cf1c3d7530f4
DiegoSHS <diegoawesome40@gmail.com>
2024-10-14 17:49:48 -0600

    Fully created DTOs

--- src/activity/activity.service.ts
+++ src/activity/activity.service.ts
@@ -8,5 +8,114 @@ export class ActivityService {
         private readonly prisma: PrismaService,
         private readonly prismaErrorHandler: PrismaErrorHandler,
     ) { }
-    
+    async create(createActivityDto: any): Promise<any> {
+        try {
+            const activity = await this.prisma.activity.create({
+                data: createActivityDto
+            })
+            return {
+                message: 'Actividad academica registrada',
+                error: null,
+                data: activity
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al crear la actividad academica')
+        }
+    }
+
+    async findAll(): Promise<any> {
+        try {
+            const activities = await this.prisma.activity.findMany()
+            if (activities.length === 0) {
+                return {
+                    message: 'No se encontraron actividades academicas',
+                    error: null,
+                    data: []
+                }
+            }
+            return {
+                message: 'Actividades academicas obtenidas con exito',
+                error: null,
+                data: activities
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al obtener las actividades academicas')
+        }
+    }
+
+    async findOne(id: string): Promise<any> {
+        try {
+            const activities = await this.prisma.activity.findMany({
+                where: {
+                    id
+                }
+            })
+            if(activities.length === 0){
+                return {
+                    message: 'No se encontro la actividad academica',
+                    error: null,
+                    data: []
+                }
+            }
+            return {
+                message: 'Actividad academica obtenida con exito',
+                error: null,
+                data: activities
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al obtener la actividad academica')
+        }
+    }
+
+    async update(id: string, updateActivityDto: any): Promise<any> {
+        try {
+            const activity = await this.prisma.activity.update({
+                where: {
+                    id,
+                },
+                data: updateActivityDto
+            })
+            return {
+                message: 'Actividad academica actualizada',
+                error: null,
+                data: activity
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al actualizar la actividad academica')
+        }
+    }
+
+    async remove(id: string): Promise<any> {
+        try {
+            const activity = await this.prisma.activity.delete({
+                where: {
+                    id
+                }
+            })
+            return {
+                message: 'Actividad academica eliminada',
+                error: null,
+                data: activity
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al eliminar la actividad academica')
+        }
+    }
+
+    async removeMany(id: number): Promise<any> {
+        try {
+            const result = await this.prisma.activity.deleteMany({
+                where:{
+                    partialTemplateId: id
+                }
+            })
+            return {
+                message: 'Se eliminaron las actividades academicas',
+                error: null,
+                data: result
+            }
+        } catch (error) {
+            return this.prismaErrorHandler.handleError(error, 'Error al eliminar las actividades academicas')
+        }
+    }
 }
