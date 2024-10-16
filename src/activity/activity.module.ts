2363a5e0abd368de690b1ea0e51a7cd0168d952a
DiegoSHS <diegoawesome40@gmail.com>
2024-10-16 12:53:32 -0600

    Added prismaErrorHandler as provider in activityModule

--- src/activity/activity.module.ts
+++ src/activity/activity.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ActivityController } from './activity.controller';
 import { ActivityService } from './activity.service';
 import { PrismaService } from 'src/prisma.service';
+import { PrismaErrorHandler } from '../common/validation/prisma-error-handler';
 
 @Module({
   controllers: [ActivityController],
-  providers: [ActivityService, PrismaService]
+  providers: [ActivityService, PrismaService, PrismaErrorHandler]
 })
 export class ActivityModule {}
