1989da4d59fd48ac6960b668e3e348f259f66101
DiegoSHS <diegoawesome40@gmail.com>
2024-11-20 17:04:11 -0600

    Adde dpublic route

--- src/personal-data/personal-data.controller.ts
+++ src/personal-data/personal-data.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/
 import { PersonalDataService } from './personal-data.service';
 import { CreatePersonalDataDto } from 'src/models/personalData/create-personal-data.dto';
 import { customBoolPipe, customIdPipe } from 'src/common/validation/custom-validation.pipe';
+import { Public } from '../auth/decorators/public.decorator';
 
 @Controller('personal-data')
 export class PersonalDataController {
@@ -16,6 +17,7 @@ export class PersonalDataController {
     createMany(@Body() createPersonalDataDto: CreatePersonalDataDto[]) {
         return this.personalDataService.createMany(createPersonalDataDto);
     }
+    @Public()
     @Get()
     find(
         @Query('id', customIdPipe) id?: number,
