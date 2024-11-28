64de180aebd8b8fffa157110ddff008555709a35
Diego López Ariza <diegoawesome2017@outlook.com>
2024-11-28 16:28:44 -0600

    Updated the JSDoc in the personal data controller

--- src/personal-data/personal-data.controller.ts
+++ src/personal-data/personal-data.controller.ts
@@ -19,14 +19,30 @@ import { Public } from '../auth/decorators/public.decorator';
 @Controller('personal-data')
 export class PersonalDataController {
   constructor(private readonly personalDataService: PersonalDataService) { }
+  /**
+   * Handles the creation of a new personal data through post request.
+   * @param createPersonalDataDto the data transfer object for creating a new personal data
+   * @returns the created personal data
+   */
   @Post()
   create(@Body() createPersonalDataDto: CreatePersonalDataDto) {
     return this.personalDataService.create(createPersonalDataDto);
   }
+  /**
+   * Creates multiple new personal data through post request.
+   * @param createPersonalDataDto The list of data transfer objects
+   * @returns the number of created personal data
+   */
   @Post('many')
   createMany(@Body() createPersonalDataDto: CreatePersonalDataDto[]) {
     return this.personalDataService.createMany(createPersonalDataDto);
   }
+  /**
+   * Retrieves a personal data by ID or all personal data if no query parameters are provided.
+   * @param id The id of the personal data to retrieve
+   * @param active The active status of the personal data to retrieve
+   * @returns returns the found personal data
+   */
   @Public()
   @Get()
   find(
@@ -36,6 +52,12 @@ export class PersonalDataController {
     if (id) return this.personalDataService.findOne(id);
     return this.personalDataService.findAll(active);
   }
+  /**
+   * Retrieves all personal data by position or area if provided, otherwise retrives all personal data.
+   * @param position The position of the personal data to retrieve
+   * @param area The area of the personal data to retrieve
+   * @returns The found personal data
+   */
   @Public()
   @Get('insensitive')
   findInsensitive(
@@ -47,6 +69,12 @@ export class PersonalDataController {
       area,
     );
   }
+  /**
+   * Updates a personal data by ID through put request.
+   * @param id The id of the personal data to update, provided in the URL
+   * @param createPersonalDataDto The new data to update the personal data with
+   * @returns The updated personal data
+   */
   @Put(':id')
   update(
     @Param('id', customIdPipe) id: number,
@@ -54,6 +82,11 @@ export class PersonalDataController {
   ) {
     return this.personalDataService.update(id, createPersonalDataDto);
   }
+  /**
+   * Deletes a personal data by ID.
+   * @param id The id of the personal data to delete, provided in the URL
+   * @returns The deleted personal data
+   */
   @Delete(':id')
   delete(@Param('id', customIdPipe) id: number) {
     return this.personalDataService.delete(id);
