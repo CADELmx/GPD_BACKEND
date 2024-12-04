c21231ba10b132bb2fc17fe0574d2ee473d06d45
DiegoSHS <diegoawesome40@gmail.com>
2024-12-04 13:11:53 -0600

    Updated partial template logic for correction

--- src/partial-templates/partial-templates.service.ts
+++ src/partial-templates/partial-templates.service.ts
@@ -332,9 +332,6 @@ export class PartialTemplatesService {
   ): Promise<any> {
     try {
       await this.validateId(id);
-      await this.validateIfExistsTemplateId(updatePartialTemplateDto);
-      await this.validateTotalByPosition(updatePartialTemplateDto);
-
       const updatePartialTemplate = await this.prisma.partialTemplate.update({
         data: { ...updatePartialTemplateDto } as any,
         where: {
